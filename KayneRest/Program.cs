801b3862c6c4c50619f73d45c922590447a9cd92
kyle jenne <strongagent1@gmail.com>
2022-11-21 11:45:54 -0500

    Kanye exercise for KJ

--- KayneRest/Program.cs
+++ KayneRest/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace KayneRest
+﻿using Newtonsoft.Json.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.WebRequestMethods;
+
+namespace KayneRest
 {
     public class Program
     {
-        static void Main(string[] args)
+        public class QuoteGenerator
         {
-            Console.WriteLine("Hello, World!");
+            private HttpClient _client;
+
+            public QuoteGenerator(HttpClient client)
+            {
+                _client = client;
+            }
+
+            static void Main(string[] args)
+            {
+                var client = new HttpClient();
+
+
+
+                for (int i = 0; i < 6; i++)
+                {
+                    var kanyeURL = "https://api.kanye.rest/";
+
+                    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                    var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+                    var ronResponse = client.GetStringAsync(ronURL).Result;
+
+                    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                    Console.WriteLine($" Ron Swanson: {ronQuote}");
+                    Thread.Sleep(2500);
+                    Console.WriteLine();
+                    Console.WriteLine($" Kanye : \"{kanyeQuote}\"");
+                    Thread.Sleep(2500);
+                    Console.WriteLine();
+
+                }
+            }
+
+
         }
     }
 }
